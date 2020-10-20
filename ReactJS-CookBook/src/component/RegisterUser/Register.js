4a6d79ed226bc0e5d0cce99df7f12e4fb4c63221
Subhakar Prabhurajendran(UST <U70350@ust-global.com>
2020-10-20 16:01:33 +0530

    commit

--- ReactJS-CookBook/src/component/RegisterUser/Register.js
+++ ReactJS-CookBook/src/component/RegisterUser/Register.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Redirect } from "react-router-dom";
-import './Login.css';
+import './Register.css';
+import axios from 'axios'
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      register: false,
-      redirect: false,
+      islogin: false,
+      userName:"",
       email:"",
       password:""
     };
@@ -20,52 +21,61 @@ class Login extends Component {
 
   onLoginClick(e){
     e.preventDefault();
-    this.setState({ redirect: true });
+    this.setState({ islogin: true });
   }
 
   onRegisterClick(e){
     e.preventDefault();
-    this.setState({ register: true });
+
+    console.log('this.state', this.state);
+
+    axios.post('https://react-cookbook-users.herokuapp.com/users/register', this.state)
+    .then(response => {
+      console.log('response', response);
+    })
+    .catch(error => {
+      console.log('error', error);
+    })
   }
 
   onChange(e) {
-    this.setState({ [e.target.type]: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   render(){
 
-    if (this.state.redirect) {
-      return <Redirect to={"/home"} />;
+    if (this.state.islogin) {
+      return <Redirect to={"/"} />;
     }
-
-    if (this.state.register) {
-      return <Redirect to={"/register"} />;
-    }
-
     return(
       <div className="container">
       <div className="jumbotron jumboContainer">
       <Form className="textLabelActive">
+        <Form.Group controlId="formBasicUserName">
+          <Form.Label>Enter your Full Name (Required):</Form.Label>
+          <Form.Control name="userName" placeholder="Enter Full Name" onChange={this.onChange}/>
+        </Form.Group>
+
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Enter your Login details</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" onChange={this.onChange}/>
+          <Form.Label>Enter your Email ID (Required):</Form.Label>
+          <Form.Control name="email" placeholder="Enter Email ID" onChange={this.onChange}/>
           <Form.Text>
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" onChange={this.onChange}/>
+          <Form.Control name="password" placeholder="Password" onChange={this.onChange}/>
         </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Remember me" />
-        </Form.Group>
-        <Button className="buttonWidth" variant="primary" type="submit" onClick={this.onLoginClick}>
-        Login
-        </Button> &nbsp; &nbsp; &nbsp;
+
         <Button className="buttonWidth" variant="primary" type="submit" onClick={this.onRegisterClick}>
         Register
+        </Button> &nbsp; &nbsp; &nbsp;
+
+        <Button className="buttonWidth" variant="primary" type="submit" onClick={this.onLoginClick}>
+        Login
         </Button>
+
       </Form>
       </div>
       </div>
--- ReactJS-CookBook/src/utilities/routes.js
+++ ReactJS-CookBook/src/utilities/routes.js
@@ -7,12 +7,14 @@ import {
 import Login from '../component/Login/Login';
 import Home from '../component/Home/Home'
 import IFSC from '../component/IFSC/IFSC'
+import RegisterUser from '../component/RegisterUser/Register'
 
 const routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Login}></Route>
       <Route path="/home" component={Home}></Route>
+      <Route path="/register" component={RegisterUser}></Route>
       <Route path="/ifsc" component={IFSC}></Route>
     </Switch>
   </BrowserRouter>
